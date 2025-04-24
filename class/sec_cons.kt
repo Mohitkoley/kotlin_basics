88301fdfa7fcb3fe554b381447f78730e3508586
Mohit Koley <mohitkoley5@gmail.com>
2025-04-24 12:33:50 +0530

    sec cons updated

--- class/sec_cons.kt
+++ class/sec_cons.kt
@@ -1,9 +1,16 @@
 fun main(){
 	val user = User("mohit")
-	println("name is ${user.name}")
+	//val user = User("Mohit", 24)
+	println("name is ${user.name} and lastName is ${user.lastName}")
 }
 
-class User(var name: String,lastName: String, age: Int){
+class User(var name: String,var lastName: String, age: Int){
 
-constructor(name: String): this(name, "lastName", 10)
+constructor(name: String): this(name, "lastName", 10){
+println("1st constructor")
+}
+
+constructor(name: String, age: Int) : this(name, "family",age){
+println("2nd constructor")
+}
 }
